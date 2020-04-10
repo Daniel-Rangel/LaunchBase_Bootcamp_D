4316265bbbd664e107cc78f9f20f633ac8baa78d
Daniel Rangel <danf.arangel@gmail.com>
2020-04-10 14:25:38 -0300

    botão de maximizar feito

--- Desafio_Front/scripts.js
+++ Desafio_Front/scripts.js
@@ -1,14 +1,25 @@
 /* chama os a box do html para ser manibulado */
 const modalOver = document.querySelector(".modal_over")
 const cards = document.querySelectorAll(".card")
+const modal = document.querySelector(".modal")
 
-/* adicionar um evendo ards*/
+/* adicionar um evendo ards*/;
 for(let card of cards){
   card.addEventListener("click", function(){
     modalOver.classList.add("active")
+
+    let verdadeiro = modal.classList.contains("max-m")
+
+    if(!verdadeiro == true){
+      document.querySelector(".ampliar").addEventListener("click", function(){
+        modal.classList.toggle("max-m")
+      })
+    }else{
+      modal.classList.remove("max-m")
+    }
+    
   })
 }
-
 /* == remove o modal */
 document.querySelector(".close-modal").addEventListener("click", function(){
   modalOver.classList.remove("active")
