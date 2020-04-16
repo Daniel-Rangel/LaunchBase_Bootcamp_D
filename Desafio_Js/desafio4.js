1f79001676888178eed3a3df07393d97a7ba4be8
Daniel Rangel <danf.arangel@gmail.com>
2020-04-16 17:13:27 -0300

    add servidor e pagina de erro

--- Desafio_Js/desafio4.js
+++ Desafio_Js/desafio4.js
@@ -24,6 +24,27 @@ function getHigherTransactionByType(typeTransaction){
   return max
 }
 
+// soma de valores
+
+function soma(objeto){
+  let somar = 0
+  let subit = 0
+  for(typ of objeto.transactions){
+    if(typ.type == "credit"){
+      somar += typ.value
+    }
+    if(typ.type == "debit"){
+      subit -= typ.value
+    }
+  }
+  console.log(somar)
+  console.log(subit)
+
+}
+
+
+
+
 // função que retorna o valor medio das transações independente do seu tipo
 function getAverageTransactionValue(){
   let averageValue = 0
@@ -58,7 +79,7 @@ createTransaction({type: 'credit', value: 120})
 createTransaction({type: 'debit', value: 80})
 createTransaction({type: 'debit', value: 30})
 
-
+soma(user)
 
 console.log(getHigherTransactionByType("credit"))
 console.log(getHigherTransactionByType("debit"))
