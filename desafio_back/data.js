05359d34c91f0fab1d77d506a4856a48b1f6a8e7
Daniel Rangel <danf.arangel@gmail.com>
2020-04-16 21:22:09 -0300

    adicionado pagina dinamica em Sobre

--- desafio_back/data.js
+++ desafio_back/data.js
@@ -3,7 +3,7 @@ module.exports = [
     id: "starter",
     nCurso: 01,
     logoImg: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
-    msg: "Tornese um programador desejado no mercado de trabalho com o curso gratuitos",
+    msg: "<span>Tornese um programador desejado</span> no mercado de trabalho com o curso gratuitos",
     modulo: "5 modulos",
     value: "Free"
   },
@@ -11,7 +11,7 @@ module.exports = [
     id: "launchbase",
     nCurso: 02,
     logoImg: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
-    msg: "Tornese um programador desejado no mercado de trabalho com o curso gratuitos",
+    msg: "<span>Tornese um programador desejado</span> no mercado de trabalho com o curso gratuitos",
     modulo: "10 modulos",
     value: "Paid out"
   },
@@ -19,7 +19,7 @@ module.exports = [
     id: "gostack",
     nCurso: 03,
     logoImg: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
-    msg: "Tornese um programador desejado no mercado de trabalho com o curso Gostack",
+    msg: "<span>Tornese um programador desejado</span> no mercado de trabalho com o curso Gostack",
     modulo: "15 modulos",
     value: "Paid out"
   }
--- desafio_back/server.js
+++ desafio_back/server.js
@@ -10,7 +10,8 @@ server.use(express.static("public"))
 server.set("view engine", "njk")
 
 nunjucks.configure("views",{
-  express:server
+  express:server,
+  autoescape: false
 })
 
 
@@ -19,11 +20,24 @@ server.get("/", function(req, res){
 })
 
 server.get("/sobre", function(req,res){
-  return res.render('about')
+  const sobre = {
+    imgLogo : 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
+    titulo : 'Rocketseat',
+    descricao : 'Mais do que uma plataforma de educação em tecnologia, somos uma comunidade incrível de programadores em busca do próximo nível',
+    tecnologias : [
+      {nome : 'JavaScript'},
+      {nome : 'Nodejs'},
+      {nome : 'ReactJS'},
+      {nome : 'React Native'}
+    ]
+  }
+  return res.render('about', {sobre})
 })
 
 server.use(function(req, res) {
   res.status(404).render("not-found");
 })
-server.listen(5000)
+server.listen(5000, function(){
+  console.log("rodando")
+})
 
