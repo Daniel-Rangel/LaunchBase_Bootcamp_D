916cd11af535587bcaec026c3d560e21675b4b3c
DanielRangel <danf.arangel@gmail.com>
2020-06-01 21:45:40 -0300

    criado data json tranferido dados do frond  para o data feito validações

--- dsModulo04/routes.js
+++ dsModulo04/routes.js
@@ -3,18 +3,18 @@ const routes = express.Router()
 const teachers = require('./teachers')
 
 routes.get('/', function(req, res){
-    return res.redirect('/instructor')
+    return res.redirect('/Teachers')
 })
 
-routes.get("/instructor", function(req, res){
+routes.get("/Teachers", function(req, res){
 return res.render('instructor/Teachers')
 })
 
-routes.get('/instructor/create', function(req,res){
+routes.get('/Teachers/create', function(req,res){
     return res.render('instructor/create')
 })
 
-routes.post('/instructor', teachers.post)
+routes.post('/Teachers', teachers.post)
 
 routes.use(function(req, res) {
 res.status(404).render("not-found");
--- dsModulo04/teachers.js
+++ dsModulo04/teachers.js
@@ -1,4 +1,39 @@
+const fs = require('fs')
+const data = require('./data.json')
 
 exports.post = function(req, res){
-    return res.send('sem valor')
+
+    const keys = Object.keys(req.body)
+    /* validaçao */
+    for(let key of keys){
+        if(req.body[key] == "") {
+            return res.send('Por favor preencher todos os campos')
+        }
+    }
+
+    /* descontruindo objeto */
+    let { avatar_url, name , birth, schooling, typeclass, school_subjects} = req.body
+    
+    birth = Date.parse(birth)
+    const id = Number(data.teachers.length + 1)
+    const create_at = Date.now()
+
+    /* savamento de dados */
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        schooling,
+        typeclass,
+        school_subjects,
+        create_at
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('erro ao requisitar dados')
+
+        return res.redirect('/Teachers')
+    })
+
 }
